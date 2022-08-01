f936cb77cb02dce57c57db4319b7f8ed072a5f1a
Ragupathy <kragu892@gmail.com>
2022-08-01 17:28:14 +0530

    mesa

---
+++ src/org/over/OverrideA.java
@@ -0,0 +1,11 @@
+package org.over;
+
+public class OverrideA {
+	public void UG() {
+		System.out.println("Under");
+	}
+     public  void PG() {
+    	 System.out.println("Master");
+     }
+
+}
---
+++ src/org/over/OverrideB.java
@@ -0,0 +1,28 @@
+package org.over;
+
+public class OverrideB extends OverrideA {
+	public void UG() {
+		System.out.println("ug");
+		
+	}
+	public void PG() {
+		System.out.println("pg");
+	}
+	public static void main(String[] args) {
+		OverrideB R =new OverrideB();
+		OverrideA RR= new OverrideA();
+		R.UG();
+		R.UG();
+		RR.PG();
+		RR.UG();
+		
+	}
+	
+
+
+
+
+
+
+
+}
